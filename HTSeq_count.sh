05042e381bd772fd197b74309038cabf80a685b8
Amin R Mohamed <37433468+AminRM@users.noreply.github.com>
2020-06-02 15:43:33 +1000

    Update HTSeq_count.sh

--- HTSeq_count.sh
+++ HTSeq_count.sh
@@ -1,3 +1,10 @@
-
-python -m HTSeq.scripts.count -a 10 --stranded=reverse -r name accepted_hits_sorted.sam /data/moh034/salmon/Salmo_salar-annotation.gff3 > Liv_T1F1_reverse.counts
+#!/bin/bash
+#SBATCH --job-name=br_counts
+#SBATCH --time=20:00:00
+#SBATCH --output dump.out
+#SBATCH --error dump.err
+#SBATCH --mem=10gb
+#SBATCH --mail-user=amin.esmail@csiro.au
+module load python
+python -m HTSeq.scripts.count -a 10 --stranded=reverse -r name ${FILE}_accepted_hits_sorted.sam /data/moh034/salmon/Salmo_salar-annotation.gff3 > ${FILE}.counts
 
