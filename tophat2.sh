402789b5d0ab8fa1f51e7f1d2e6de6861459eb55
Amin R Mohamed <37433468+AminRM@users.noreply.github.com>
2020-06-02 15:18:36 +1000

    Update tophat2.sh

--- tophat2.sh
+++ tophat2.sh
@@ -11,5 +11,5 @@ module load bowtie
 module load tophat
 module load samtools
 
-tophat2  --output-dir mapping /data/moh034/salmon/salmon_ref ${FILE}_R1.fastq.gz -2 ${FILE}_R2.fastq.gz
+tophat2 -p 8 --output-dir mapping /data/moh034/salmon/salmon_ref ${FILE}_R1.fastq.gz -2 ${FILE}_R2.fastq.gz
 
