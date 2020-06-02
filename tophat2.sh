cb4d1df06f6543792ed7a807fb0c3c063c00960f
Amin R Mohamed <37433468+AminRM@users.noreply.github.com>
2020-06-02 15:17:30 +1000

    Create tophat2.sh

---
+++ tophat2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=hisat-mapping
+#SBATCH --output dump.out
+#SBATCH --error dump.err
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=20gb
+#SBATCH --time=20:00:00
+#SBATCH --mail-user=amin.esmail@csiro.au
+
+module load bowtie
+module load tophat
+module load samtools
+
+tophat2  --output-dir mapping /data/moh034/salmon/salmon_ref ${FILE}_R1.fastq.gz -2 ${FILE}_R2.fastq.gz
+
